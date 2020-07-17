54bae98eeb1dc8b5f565e668d681d1f80b2150f1
Jon Reyes <jonrey3@gmail.com>
2020-07-17 02:11:07 +0000

    Implemented User Stats Page

--- ticky_check.py
+++ ticky_check.py
@@ -6,6 +6,8 @@ import csv
 os.system("cat syslog.log > temp.log")
 f = open("temp.log","r+")
 errors = []
+userstats = {}
+m = []
 for line in f.readlines():
 	match = re.search("(INFO|ERROR).*\(.*\)",line).group()
 	type = re.search("(INFO|ERROR)",match).group()
@@ -22,9 +24,13 @@ for line in f.readlines():
 	message = " ".join(message)
 	print(message)
 	print(number)
-	print(username)
+	#print(username)
 	if type == "ERROR":
 		errors.append(message)
+	username = username[1:len(username)-1]
+	print(username)
+	userstats[username] = {"INFO":0,"ERROR":0}
+	m.append(match)
 f.close()
 errorcount = {}
 for error in errors:
@@ -38,3 +44,18 @@ with open("error_message.csv","w",newline="") as em_file:
 	for key,value in errorcount:
 		writer.writerow([key,value])
 em_file.close()
+for item in m:
+	type = re.search("(INFO|ERROR)",item).group()
+	username = re.search("\(.*\)",item).group()
+	username = username[1:len(username)-1]
+	userstats[username][type] += 1
+userstats = sorted(userstats.items())
+with open("user_statistics.csv","w",newline="") as us_file:
+	writer = csv.writer(us_file)
+	writer.writerow(["Username","INFO","ERROR"])
+	for item in userstats:
+		username = item[0]
+		ic = item[1]["INFO"]
+		ec = item[1]["ERROR"]
+		writer.writerow([username,ic,ec])
+us_file.close()
